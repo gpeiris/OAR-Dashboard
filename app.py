f60c39f10a8f0e774d04439a3925a5a35202345f
gpeiris <56284756+gpeiris@users.noreply.github.com>
2022-09-06 17:52:29 +1000

    Update app.py

--- app.py
+++ app.py
@@ -10,10 +10,10 @@ df = pd.read_csv("OARMetricComp.csv")  # csv file
 
 fig = px.scatter(
     df,
-    x='V5Gy',            # x-axis metric
-    y='Volume',          # y-axis metric
-    size='CI_RTOG100',   # size of data point (don't use )
-    color='PlanID',      # colour of data point
+    x='V25Gy',         # x-axis metric
+    y='PlanID',        # y-axis metric
+    size='Volume',     # size of data point (don't use )
+    color='OAR',       # colour of data point
     hover_name='OAR',
     #log_x=True,
     size_max=60,
